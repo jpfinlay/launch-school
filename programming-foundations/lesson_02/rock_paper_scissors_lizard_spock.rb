783283a57f1a168a173ae40c357f44b71e08fe07
Jamie Finlay <jpfinlay@gmail.com>
2016-03-23 19:53:15 +0000

    Complete RPS bonus features 1 and 3.

--- programming-foundations/lesson_02/rock_paper_scissors_lizard_spock.rb
+++ programming-foundations/lesson_02/rock_paper_scissors_lizard_spock.rb
@@ -6,9 +6,6 @@
 
 VALID_CHOICES = %w(rock paper scissors lizard spock).freeze
 
-player_score = 0
-computer_score = 0
-
 def prompt(message)
   puts "=> #{message}"
 end
@@ -38,35 +35,43 @@ end
 
 def who_scores?(player, computer)
   if win?(player, computer)
-    player_score
+    $player_score += 1
   elsif win?(computer, player)
-    computer_score
+    $computer_score += 1
   else
     prompt("No score.")
   end
 end
 
 loop do # main loop
-  choice = ''
+  $player_score = 0     # Ugh! I don't want to use global variables, but can't
+  $computer_score = 0   # think of another way without Classes and instance variables.
 
   puts "Welcome to Rock, Paper, Scissors, Lizard, Spock!"
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}.")
-    choice = gets.chomp
 
-    VALID_CHOICES.include?(choice) ? break : prompt('Invalid choice. Try again.')
+  until $player_score == 5 || $computer_score == 5
+    choice = ''
+
+
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}.")
+      choice = gets.chomp
+
+      VALID_CHOICES.include?(choice) ? break : prompt('Invalid choice. Try again.')
+    end
+
+    computer_choice = VALID_CHOICES.sample
+
+    who_scores?(choice, computer_choice)
+
+    display_result(choice, computer_choice)
+
+    puts("You: #{$player_score} - #{$computer_score} Computer")
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  who_scores?(choice, computer_choice)
-
-  display_result(choice, computer_choice)
-
-  puts("You: #{player_score} - #{computer_score} Computer")
 
   prompt('Play again?')
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
+
 prompt("Thank you for playing. Good bye!")
