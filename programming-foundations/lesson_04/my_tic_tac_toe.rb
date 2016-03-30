5998481a7db3294b0e3070466df8a302701e95c7
Jamie Finlay <jpfinlay@gmail.com>
2016-03-30 23:19:22 +0100

    Continue working on tic_tac_toe.rb. Update time sheet.

--- programming-foundations/lesson_04/my_tic_tac_toe.rb
+++ programming-foundations/lesson_04/my_tic_tac_toe.rb
@@ -1,52 +1,30 @@
 # my_tic_tac_toe.rb
-# A tic-tac-toe application
+# A tic-tac-toe Ruby game app.
 
-# 1. Display the initial empty 3x3 board.
-# 2. Ask the user to mark a square.
-# 3. Computer marks a square.
-# 4. Display the updated board state.
-# 5. If winner, display winner, ELSE got to #2
-# 6. If board is full, display tie. => if initial array has no elements left
-# 7. If neither winner nor board is full, go to #2
-# 8. Play again?
-# 9. If yes, go to #1
-# 10. Good bye!
+def prompt(message)
+  print("=> #{message} ")
+end
 
-# Set up board and winning combinations
-valid_squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-winning_combinations = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [1, 4, 7],
-  [2, 5, 8],
-  [3, 6, 9],
-  [1, 5, 9],
-  [3, 5, 9]
-]
-
-# Square selection logic
-picks = []
-
-def pick(valid_squares, pick, picks)
-  if valid_squares.include?(pick)
-    picks << pick
-    valid_squares[pick - 1] = "X"
+def valid_pick?(valid_picks, input, picks)
+  input = input.to_i
+  if valid_picks.include?(input)
+    picks[:player] << input
+    valid_picks.delete(input)
   else
-    "Not a valid selection, please select a different square."
+    false
   end
 end
 
-def display_board(valid_squares)
+def display_board(valid_picks)
   puts "\n\n"
-  valid_squares.each_slice(3) { |slice| p slice.join('|') }
+  valid_picks.each_slice(3) { |slice| p slice.join('|') }
   # puts "|1|2|3|".center(50)
   # puts "|4|5|6|".center(50)
   # puts "|7|8|9|".center(50)
 end
 
-def list_valid_squares(valid_squares)
-  valid_squares.reject { |e| e == "X" || e == "O" }.join(', ')
+def list_remaining_board_numbers(valid_picks)
+  valid_picks.reject { |e| e == "X" || e == "O" }.join(', ')
 end
 
 def integer?(input)
@@ -57,30 +35,80 @@ def number?(input)
   integer?(input)
 end
 
-def won?(winning_combinations, picks)
-  winning_combinations.each do |combination|
-    if (combination <=> picks.sort) == 0
-      p "won"
+def winner?(numbers, winning_combinations)
+  p numbers
+  winning_combinations.each do |combi|
+    if (combi <=> numbers.sort) == 0
+      break true
     else
+      false
+    end
+  end
+  return false
+end
+
+def display_result(picks, winning_combinations)
+  if winner?(picks[:player], winning_combinations)
+    prompt("YOU WON!")
+  elsif winner?(picks[:computer], winning_combinations)
+    prompt("COMPUTER WON!")
+  else
+    prompt("IT'S A TIE!")
+  end
+end
+
+loop do # main outer loop
+
+  # Set up board and winning combinations
+  valid_picks = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  winning_combinations = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 9]
+  ]
+
+  display_board(valid_picks)
+
+  # Track player and computer number selections
+  picks = { player: [], computer: [] }
+  round = 0
+
+  loop do # main inner loop
+    input = ''
+
+    loop do # Get players input
+      puts "\n"
+      prompt("Select a square (#{list_remaining_board_numbers(valid_picks)}): ")
+      input = gets.chomp
+
+      number?(input) && valid_pick?(valid_picks, input, picks) ? break : prompt('Invalid choice. Try again.')
+    end
+
+    # Get the computer's choice
+    computer_pick = valid_picks.sample
+    picks[:computer] << computer_pick
+    valid_picks.delete(computer_pick)
+
+    puts "Selections so far: #{picks}"
+
+    round += 1
+    display_board(valid_picks)
+
+    if round == 3 || round == 4
+      display_result(picks, winning_combinations)
+    elsif round > 4
+      prompt("IT'S A TIE!")
       break
     end
   end
+
+  prompt('Play again?')
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
-
-loop do # main loop
-  input = ''
-
-  loop do
-    display_board(valid_squares)
-    puts "\n"
-    print "Select a square (#{list_valid_squares(valid_squares)}): "
-    input = gets.chomp.to_i
-
-    pick(valid_squares, input, picks)
-    #valid_squares.include?(input.to_s) ? break : "Invalid choice"
-    p valid_squares
-    p picks
-  end
-
-  break unless won? == false
-end
+prompt("Thank you for playing. Good bye!")
