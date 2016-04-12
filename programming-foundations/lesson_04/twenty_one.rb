d579f7ba2833bdf6e99426d3b942890d5fee6d08
Jamie Finlay <jpfinlay@gmail.com>
2016-04-12 15:49:43 +0100

    Continue with Twenty One game.

--- programming-foundations/lesson_04/twenty_one.rb
+++ programming-foundations/lesson_04/twenty_one.rb
@@ -2,17 +2,16 @@ require 'pry'
 
 RANKS = %w(2 3 4 5 6 7 8 9 10 J Q K A).freeze
 SUITS = %w(C D H S).freeze
-# CARD_VALUES = {
-#   2: 2,
-#   3: 3,
-#   4: 4,
-#   "2": 2,
-#   "2": 2,
-#   "2": 2,
-#   "2": 2,
-#   "2": 2,
-#   "2": 2,
-# }
+
+def display_hands(player, dealer, count)
+  system 'clear'
+  puts ""
+  puts "TWNETY ONE".center(50,"-")
+  puts ""
+  puts "You have: #{player.flatten.join(', ')} (TOTAL: #{count(player.flatten)})"
+  puts ""
+  puts "Dealer's hand: #{dealer[0][0]}, HIDDEN"
+end
 
 player = []
 dealer = []
@@ -24,18 +23,18 @@ def initialize_deck
   deck
 end
 
-def deal(player, deck)
+def deal(deck)
   deck.shift(2)
 end
 
-def hit(deck)
-  deck.shift
+def hit(hand, deck)
+  hand << deck.shift
 end
 
 def count(hand)
   score = 0
-  hand.each do |card|
-    if card[0] == "J" || card[0] == "Q" || card[0] == "K"
+  hand.flatten.each do |card|
+    if card[0] == "J" or card[0] == "Q" or card[0] == "K"
       score += 10
     elsif card[0] == "A" && score <= 10
       score += 11 || score += 1
@@ -53,24 +52,32 @@ def bust?(hand)
   return true if score > 21
 end
 
-def show_cards(hand)
-  hand.flatten!
+def hit_or_stay?(player, dealer, deck)
+  display_hands(player, dealer, count(player))
+  
+  loop do
+    puts "=> (h)it or (s)tay?"
+    input = gets.chomp.downcase
+    if input.start_with?('h')
+      player << deck.shift
+    elsif input.start_with?('s')
+      break puts "Stay on score."
+    else
+      puts "Sorry, that is not a valid choice."
+    end
+    display_hands(player, dealer, count(player))
+  end
 end
 
-deck = initialize_deck
+# def show_cards(hand)
+#   hand.flatten!
+# end
 
-player << deal(player, deck)
-player << hit(deck)
+loop do # main game loop
+  deck = initialize_deck
 
-dealer << deal(dealer, deck)
+  player << deal(deck)
+  dealer << deal(deck)
 
-
-puts "Player's hand: #{show_cards(player).join(', ')}: Score #{count(player)}"
-puts "Dealer's hand: #{dealer[0][0]}, HIDDEN: Score #{count(dealer)}"
-
-loop do 
-  puts "(h)it or (s)tay?"
-  input = gets.chomp.downcase
-  break if input == "h" || input == "s"
-  puts "Please input an 'h' to hit or a 's' to stay."
+  hit_or_stay?(player, dealer, deck)
 end
