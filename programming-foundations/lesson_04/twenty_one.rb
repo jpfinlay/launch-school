e58afdad26f55142dc528f6075fa71496b17bfde
Jamie Finlay <jpfinlay@gmail.com>
2016-04-13 15:17:36 +0000

    Complete first iteration of Twenty-One game.

--- programming-foundations/lesson_04/twenty_one.rb
+++ programming-foundations/lesson_04/twenty_one.rb
@@ -6,9 +6,9 @@ SUITS = %w(C D H S).freeze
 def display_hands(player, dealer)
   system 'clear'
   puts ""
-  puts " T W E N T Y  O N E ".center(80,"-")
+  puts " T W E N T Y  O N E ".center(80, "-")
   puts ""
-  puts "You have: #{player.flatten.join(', ')} (TOTAL: #{count(player.flatten)})"
+  puts "You have: #{show_cards(player)} (TOTAL: #{count(player.flatten)})"
   puts ""
   puts "Dealer's hand: #{dealer[0][0]}, HIDDEN"
   puts ""
@@ -32,20 +32,32 @@ def hit(hand, deck)
   hand << deck.shift
 end
 
+def show_cards(hand)
+  hand.flatten.join('-')
+end
+
+def remove_suits(hand)
+  hand.flatten!
+  hand.each do |card|
+    card.gsub!(/[CDHS]/, '')
+  end
+end
+
 def count(hand)
   score = 0
-  hand.flatten.each do |card|
-    if card[0] == "J" or card[0] == "Q" or card[0] == "K"
+  cards = remove_suits(hand)
+  cards.each do |card|
+    if card =~ /[JQK]/
       score += 10
-    elsif card[0] == "A" && score <= 10
-      score += 11 || score += 1
-    elsif card[0] == "A" && score > 10
+    elsif card == "A" && score <= 10
+      score += 11
+    elsif card == "A" && score > 10
       score += 1
     else
-      score += card[0].to_i
+      score += card.to_i
     end
   end
-  return score
+  score
 end
 
 def bust?(hand)
@@ -54,11 +66,28 @@ def bust?(hand)
 end
 
 def hit!(hand, deck)
-  hand << deck.shift 
+  hand << deck.shift
 end
 
-def show_cards(hand)
-  hand.flatten.join('-')
+def dealer_choice(dealer, deck)
+  hit!(dealer, deck) unless count(dealer) > 17
+end
+
+def winner?(player, dealer)
+  winner = if bust?(player)
+             "You're bust. Dealer won!"
+           elsif bust?(dealer)
+             "Dealer bust. You won!"
+           elsif count(player) > count(dealer)
+             "You won!"
+           elsif count(player) < count(dealer)
+             "Dealer won!"
+           else
+             "It's a tie."
+           end
+  puts "#{winner}."
+  puts "Your hand was: #{show_cards(player)}, total #{count(player)}."
+  puts "Dealer's hand was: #{show_cards(dealer)}, total #{count(dealer)}."
 end
 
 deck = initialize_deck
@@ -68,37 +97,23 @@ dealer << deal(deck)
 
 display_hands(player, dealer)
 
+input = ''
 loop do
   display_hands(player, dealer)
-  
   puts "=> (h)it or (s)tay?"
   input = gets.chomp.downcase
-  
   player << deck.shift if input.start_with?('h')
   break if bust?(player)
-  
-  if input.start_with?('s')
-    "Player stays on #{count(player)}."
-    break
-  end 
-  #puts "Sorry, that is not a valid choice."
+  break if input.start_with?('s')
 end
 
 display_hands(player, dealer)
 
 if bust?(player)
-  puts "".center(80, '-')
-  puts "BUST! You lost :("
-  puts "Your hand was: #{show_cards(player)}, total #{count(player)}."
-  puts "Dealer's hand was: #{show_cards(dealer)}, total #{count(dealer)}."
-  puts "Good bye."
-else 
-  puts "Player has: #{count(player)}."
-  puts "Dealer has: #{show_cards(dealer)}."
+  winner?(player, dealer)
+else
+  while count(dealer) < count(player) && count(dealer) < 17
+    dealer << deck.shift
+  end
+  winner?(player, dealer)
 end
-  
-# loop do
-#   display_hands(player, dealer)
-#   break unless count(dealer) < 17
-#   dealer << deck.shift
-# end
